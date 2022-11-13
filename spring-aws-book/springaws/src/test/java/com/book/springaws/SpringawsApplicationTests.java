8b9a5cfaecde94df42dcdfff8844706cd54b6a6b
insightp25 <insightplug@gmail.com>
2022-11-13 18:56:45 +0900

    spring-aws-book first commit

---
+++ spring-aws-book/springaws/src/test/java/com/book/springaws/SpringawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.springaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
