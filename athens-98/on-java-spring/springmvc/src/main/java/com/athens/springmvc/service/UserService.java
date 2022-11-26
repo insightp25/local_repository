c1add243b20c8b7849cad687f9579dad9b74a089
insightp25 <insightplug@gmail.com>
2022-11-26 22:43:36 +0900

    security pt3 update

--- athens-98/on-java-spring/springmvc/src/main/java/com/athens/springmvc/service/UserService.java
+++ athens-98/on-java-spring/springmvc/src/main/java/com/athens/springmvc/service/UserService.java
@@ -4,32 +4,39 @@ import com.athens.springmvc.dto.SignupRequestDto;
 import com.athens.springmvc.entity.User;
 import com.athens.springmvc.entity.UserRoleEnum;
 import com.athens.springmvc.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-//@RequiredArgsConstructor
 @Service
 public class UserService {
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private static final String ADMIN_TOKEN = "AAABnv/xRVklrnYxKZ0aHgTBcXukeZygoC";
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
-    public void registerUser(SignupRequestDto requestDto) {
-        String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
 
+    public void registerUser(SignupRequestDto requestDto) {
+        // 회원 ID 중복 확인
+        String username = requestDto.getUsername();
         Optional<User> found = userRepository.findByUsername(username);
         if (found.isPresent()) {
-            throw new IllegalArgumentException("중복된 사용자 ID가 존재합니다.");
+            throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
         }
 
+        // 패스워드 암호화
+        String password = passwordEncoder.encode(requestDto.getPassword());
         String email = requestDto.getEmail();
+
+        // 사용자 ROLE 확인
         UserRoleEnum role = UserRoleEnum.USER;
         if (requestDto.isAdmin()) {
             if (!requestDto.getAdminToken().equals(ADMIN_TOKEN)) {
--- athens-98/on-java-spring/springmvc/src/main/java/security/WebSecurityConfig.java
+++ athens-98/on-java-spring/springmvc/src/main/java/security/WebSecurityConfig.java
@@ -1,25 +1,57 @@
 package security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        // h2-console 사용에 대한 허용(CSRF, FrameOptions 무시)
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // 회원 관리 처리 API (POST /user/**)에 대해 CSRF 무시
+        http.csrf()
+                .ignoringAntMatchers("/user/**");
+
         http.authorizeRequests()
+                // image 폴더를 login 없이 허용
                 .antMatchers("/images/**").permitAll()
+                // css 폴더를 login 없이 허용
                 .antMatchers("/css/**").permitAll()
+                // 회원 관리 처리 api 전부를 login 없이 허용
+                .antMatchers("/user/**").permitAll()
+                // 그 외 어떤 요청이든 '인증'
                 .anyRequest().authenticated()
                 .and()
+                // login 기능
                 .formLogin() // 로그인 기능작동
                 .loginPage("/user/login") // 사용자 정의 페이지 경로
+                .loginProcessingUrl("/user/login")
                 .defaultSuccessUrl("/") // 로그인 성공 시 이동 페이지 경로
                 .failureUrl("/user/login?error") // 로그인 실패 시 이동 페이지 경로
+                .permitAll()
+                .and()
+                // logout 기능
+                .logout()
+                .logoutUrl("/user/logout")
                 .permitAll();
     }
 }
