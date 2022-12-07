bc2b410773446a281cf4ccbd6940ea14a5084a62
insightp25 <insightplug@gmail.com>
2022-12-07 23:44:43 +0900

    assignment1 controller partial update

--- athens-98/on-java-spring/assignment1/src/main/java/com/athens/flight98/assignment1/dto/PostRequestDto.java
+++ athens-98/on-java-spring/assignment1/src/main/java/com/athens/flight98/assignment1/dto/PostRequestDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public class PostRequestDto {
     private String title;
-    private String content;
     private String author;
+    private String content;
     private String password;
 }
--- athens-98/on-java-spring/assignment1/src/main/java/com/athens/flight98/assignment1/repository/PostRepository.java
+++ athens-98/on-java-spring/assignment1/src/main/java/com/athens/flight98/assignment1/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.athens.flight98.assignment1.repository;
 
+import com.athens.flight98.assignment1.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository {
+public interface PostRepository extends JpaRepository<Long, Post> {
 }
