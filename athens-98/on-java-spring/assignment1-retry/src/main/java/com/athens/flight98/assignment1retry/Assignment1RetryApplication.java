91d781c8a1872c57b5df165e22e84f48ba22ae7a
insightp25 <insightplug@gmail.com>
2022-12-10 22:11:52 +0900

    assignment1 retry init setting

---
+++ athens-98/on-java-spring/assignment1-retry/src/main/java/com/athens/flight98/assignment1retry/Assignment1RetryApplication.java
@@ -0,0 +1,13 @@
+package com.athens.flight98.assignment1retry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1RetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1RetryApplication.class, args);
+	}
+
+}
---
+++ athens-98/on-java-spring/assignment1-retry/src/test/java/com/athens/flight98/assignment1retry/Assignment1RetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athens.flight98.assignment1retry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1RetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
