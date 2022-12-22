a36ff2280c2b724ccb00e0844e259900466ff91c
insightp25 <insightplug@gmail.com>
2022-12-22 19:42:12 +0900

    assingment1 re-try init

--- athens-98/on-java-spring/assignment1-retry/src/main/java/com/athens/flight98/assignment1retry/dto/PasswordDto.java
+++
@@ -1,9 +0,0 @@
-package com.athens.flight98.assignment1retry.dto;
-
-import lombok.Getter;
-
-@Getter
-// @Setter? @NoArgsConstructor? @AllArgsConstructor?
-public class PasswordDto {
-    private String password;
-}
