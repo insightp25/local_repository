a9cb2aa28611e5db2c97208f49577148cb39a3ca
insightp25 <insightplug@gmail.com>
2022-12-27 21:07:42 +0900

    study exercise0 finish

--- java-plygrnd-tdd-cleancode/java-baseball-playground/src/test/java/study/SetTest.java
+++ java-plygrnd-tdd-cleancode/java-baseball-playground/src/test/java/study/SetTest.java
@@ -2,11 +2,15 @@ package study;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -25,4 +29,19 @@ public class SetTest {
         int size = numbers.size();
         assertThat(size).isEqualTo(4);
     }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 9})
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    
+    @ParameterizedTest
+    @CsvSource({"1, true", "2, true", "3, true", "9, true", "4, false", "5, false"})
+    void contains(int input, String expected) {
+        assertThat(numbers.contains(input) ? "true" : "false").isEqualTo(expected);
+    }
+
 }
