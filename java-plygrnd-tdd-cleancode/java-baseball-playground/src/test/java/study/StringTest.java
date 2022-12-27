ad83f4f576f6e82bfa2e62a92575e104053c431f
insightp25 <insightplug@gmail.com>
2022-12-27 20:22:25 +0900

    study exercise1

--- java-plygrnd-tdd-cleancode/java-baseball-playground/src/test/java/study/StringTest.java
+++ java-plygrnd-tdd-cleancode/java-baseball-playground/src/test/java/study/StringTest.java
@@ -1,13 +1,51 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void splitTest() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1", "2");
+    }
+
+    @Test
+    void oneCharSplitTest() {
+        String[] actual = "1".split(",");
+        assertThat(actual).contains("1");
+    }
+
+    @Test
+    void parenthesisRemoveTest() {
+        String str = "\"(1,2)\"";
+        int openingP = str.indexOf("(");
+        int closingP = str.lastIndexOf(")");
+        String actual = str.substring(0, openingP) + str.substring(openingP + 1, closingP) + str.substring(closingP + 1);
+        assertThat(actual).isEqualTo("\"1,2\"");
+    }
+
+    @DisplayName("charAt() exception test")
+    @Test
+    void findCharExceptionTest() {
+        String str1 = "abc";
+        int input = 99;
+
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    throw new StringIndexOutOfBoundsException(str1.charAt(input));
+                }).withMessageMatching("String index out of range: 99");
+
+    }
+
 }
