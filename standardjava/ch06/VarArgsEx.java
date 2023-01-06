8e5ffdaf2ee0f55b9b35a872cf14da02e60e420f
insightp25 <insightplug@gmail.com>
2023-01-06 23:59:39 +0900

    ch6

---
+++ standardjava/ch06/VarArgsEx.java
@@ -0,0 +1,30 @@
+package ch06;
+
+public class VarArgsEx {
+    public static void main(String[] args) {
+        String[] strArr = {"aaa", "bbb", "ccc"};
+
+        System.out.println(concatenate("", "aaa", "bbb", "ccc"));
+        System.out.println(concatenate("-", strArr));
+        System.out.println(concatenate(",", new String[]{"www", "xxx", "yyy"}));
+        System.out.println("[" + concatenate(",", new String[0] + "]"));
+        System.out.println("[" + concatenate("," + "]"));
+    }
+
+    static String concatenate(String delim, String... args) {
+        String result = "";
+
+        for (String str : args) {
+            result += str + delim;
+        }
+
+        return result;
+    }
+
+    /*
+    static String concatenate(String... args) {
+        return concatenate("", args);
+    }
+    */
+
+}
