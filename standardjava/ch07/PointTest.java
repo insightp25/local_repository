05b048c6e3ad96d27df60e767dbbd0273512849e
insightp25 <insightplug@gmail.com>
2023-01-07 23:45:37 +0900

    ch07 init and few tests

---
+++ standardjava/ch07/PointTest.java
@@ -0,0 +1,40 @@
+package ch07;
+
+class PointTest {
+    public static void main(String[] args) {
+        Point3D p3 = new Point3D(1, 2, 3);
+        String location = p3.getLocation();
+        System.out.println(location);
+    }
+}
+
+class Point {
+    int x, y;
+
+    Point() {
+    }
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    String getLocation() {
+        return "x: " + x + ", y: " + y;
+    }
+}
+
+class Point3D extends Point{
+    int z;
+
+    Point3D(int x, int y, int z) {
+//        super(x, y);
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    String getLocation() {
+        return "x: " + x + ", y: " + y + ", z: " + z;
+    }
+}
