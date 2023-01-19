63b5e4036597d71614add4157996d1aa9dd2cb5d
insightp25 <insightplug@gmail.com>
2023-01-19 22:39:38 +0900

    few minor updates

--- standardjava/ch07/ParserTest.java
+++ standardjava/ch07/ParserTest.java
@@ -1,6 +1,8 @@
 package ch07;
 
 
+import java.util.*;
+
 interface Parseable {
     public abstract void parse(String fileName);
 }
@@ -29,11 +31,19 @@ class ParseManager {
 }
 
 
+
 public class ParserTest {
     public static void main(String[] args) {
         Parseable parser = ParseManager.getParser("XML");
         parser.parse("document.xml");
         parser = ParseManager.getParser("HTML");
         parser.parse("document2.html");
+
+        Map<?, ?> wow;
+        List<?> wow2;
+        Set<?> wow3;
+        Vector<?> wow4;
+        AbstractList<?> wow5;
+        SortedMap<?, ?> wow7;
     }
 }
