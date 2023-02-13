7f4be1709de9fa41d266c89be0a2dd66fb09477d
insightp25 <insightplug@gmail.com>
2023-02-13 23:44:57 +0900

    mvc1 servlet webpage simple exercise

--- inflearn-kimyounghan/mvc1/servlet/src/main/java/hello/servlet/ServletInitializer.java
+++ inflearn-kimyounghan/mvc1/servlet/src/main/java/hello/servlet/ServletInitializer.java
@@ -1,6 +1,6 @@
-package haro.servlet;
+package hello.servlet;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import   org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
