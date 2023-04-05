9fc6c0e7c437de52d32778c538bf51fbeb31ddc1
insightp25 <insightplug@gmail.com>
2023-04-05 19:38:39 +0900

    first commit

---
+++ community-sns/src/main/java/mock/communitysns/CommunitySnsApplication.java
@@ -0,0 +1,13 @@
+package mock.communitysns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitySnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunitySnsApplication.class, args);
+	}
+
+}
---
+++ community-sns/src/test/java/mock/communitysns/CommunitySnsApplicationTests.java
@@ -0,0 +1,13 @@
+package mock.communitysns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitySnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
