daf00c6a17c79a998cc7b2c73cb8b030588c9674
insightp25 <insightplug@gmail.com>
2023-05-20 15:34:26 +0900

    add create events test

---
+++ whiteship/demo-inflearn-rest-api/src/test/java/me/rmndr/demoinflearnrestapi/events/EventControllerTests.java
@@ -0,0 +1,26 @@
+package me.rmndr.demoinflearnrestapi.events;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+public class EventControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void createdEvent() throws Exception {
+        mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON))
+            .andExpect(status().isCreated());
+    }
+}
