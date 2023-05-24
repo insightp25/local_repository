d708a5cc0c43870f18f3bf5f1b8da7d4f6783792
insightp25 <insightplug@gmail.com>
2023-05-24 17:46:37 +0900

    Ristrict input for POST /api/events

--- whiteship/demo-inflearn-rest-api/src/main/java/me/rmndr/demoinflearnrestapi/events/EventController.java
+++ whiteship/demo-inflearn-rest-api/src/main/java/me/rmndr/demoinflearnrestapi/events/EventController.java
@@ -3,7 +3,7 @@ package me.rmndr.demoinflearnrestapi.events;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 import java.net.URI;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,15 +16,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EventController {
     private final EventRepository eventRepository;
 
-    public EventController(EventRepository eventRepository) {
+    private final ModelMapper modelMapper;
+
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper) {
         this.eventRepository = eventRepository;
+        this.modelMapper = modelMapper;
     }
 
 //    @Autowired
 //    EventRepository eventRepository;
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event) {
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+//        Event event = Event.builder()
+//            .name(eventDto.getName())
+//            .description(eventDto.getDescription())
+//            .beginEnrollmentDateTime(eventDto.getBeginEnrollmentDateTime())
+//            .closeEnrollmentDateTime(eventDto.getCloseEnrollmentDateTime())
+//            .beginEventDateTime(eventDto.getBeginEventDateTime())
+//            .endEventDateTime(eventDto.getEndEventDateTime())
+//            .location(eventDto.getLocation())
+//            .basePrice(eventDto.getBasePrice())
+//            .maxPrice(eventDto.getMaxPrice())
+//            .limitOfEnrollment(eventDto.getLimitOfEnrollment())
+//            //.eventStatus(EventStatus.DRAFT)
+//            .build();
+        Event event = modelMapper.map(eventDto, Event.class);
+
         Event newEvent = this.eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
         return ResponseEntity.created(createdUri).body(event);
--- whiteship/demo-inflearn-rest-api/src/test/java/me/rmndr/demoinflearnrestapi/events/EventControllerTests.java
+++ whiteship/demo-inflearn-rest-api/src/test/java/me/rmndr/demoinflearnrestapi/events/EventControllerTests.java
@@ -8,17 +8,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
+//@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
@@ -26,12 +31,13 @@ public class EventControllerTests {
     @Autowired
     ObjectMapper objectMapper;
 
-    @MockBean
-    EventRepository eventRepository;
+//    @MockBean
+//    EventRepository eventRepository;
 
     @Test
     public void createdEvent() throws Exception {
         Event event = Event.builder()
+            .id(100)
             .name("Spring")
             .description("REST API Development with Srping")
             .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
@@ -42,9 +48,12 @@ public class EventControllerTests {
             .maxPrice(200)
             .limitOfEnrollment(100)
             .location("강남역 D2 스타트업 팩토리")
+            .free(true)
+            .offline(false)
+            .eventStatus(EventStatus.PUBLISHED)
             .build();
-        event.setId(10);
-        Mockito.when(eventRepository.save(event)).thenReturn(event);
+//        event.setId(10);
+//        Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -53,9 +62,10 @@ public class EventControllerTests {
             .andDo(print())
             .andExpect(status().isCreated())
             .andExpect(jsonPath("id").exists())
-//            .andExpect(header().exists("Location"))
-            .andExpect(header().exists(HttpHeaders.LOCATION))
-//            .andExpect(header().string("Content-Type", "application/hal+json;charset=UTF-8"));
-            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE + ";charset=UTF-8"));
+            .andExpect(header().exists(HttpHeaders.LOCATION)) // from .andExpect(header().exists("Location"))
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE + ";charset=UTF-8"))  // from .andExpect(header().string("Content-Type", "application/hal+json;charset=UTF-8"));
+            .andExpect(jsonPath("id").value(Matchers.not(100)))
+            .andExpect(jsonPath("free").value(Matchers.not(true)))
+            .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
     }
 }
