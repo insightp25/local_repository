20b88effa358bb451d6eaecda0b70cc7823c1677
JO <71416000+insightp25@users.noreply.github.com>
2023-06-03 04:02:52 +0900

    validate wrong data

--- whiteship/demo-inflearn-rest-api/src/main/java/me/rmndr/demoinflearnrestapi/events/EventController.java
+++ whiteship/demo-inflearn-rest-api/src/main/java/me/rmndr/demoinflearnrestapi/events/EventController.java
@@ -5,10 +5,10 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import java.net.URI;
 import javax.validation.Valid;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.internal.Errors;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +20,12 @@ public class EventController {
 
     private final ModelMapper modelMapper;
 
-    public EventController(EventRepository eventRepository, ModelMapper modelMapper) {
+    private final EventValidator eventValidator;
+
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper, EventValidator eventValidator) {
         this.eventRepository = eventRepository;
         this.modelMapper = modelMapper;
+        this.eventValidator = eventValidator;
     }
 
 //    @Autowired
@@ -33,6 +36,8 @@ public class EventController {
         if (errors.hasErrors()) {
             return ResponseEntity.badRequest().build();
         }
+
+        eventValidator.validate(eventDto, errors);
 //        Event event = Event.builder()
 //            .name(eventDto.getName())
 //            .description(eventDto.getDescription())
@@ -46,8 +51,11 @@ public class EventController {
 //            .limitOfEnrollment(eventDto.getLimitOfEnrollment())
 //            //.eventStatus(EventStatus.DRAFT)
 //            .build();
-        Event event = modelMapper.map(eventDto, Event.class);
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
 
+        Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = this.eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
         return ResponseEntity.created(createdUri).body(event);
