f3c30f2356074a4f14eab7704f9eec35cda94f1f
JO <71416000+insightp25@users.noreply.github.com>
2023-07-05 00:28:07 +0900

    On advisor, bean post-processor

---
+++ spring-backend-kyh/spring-advanced/advanced/src/test/java/haro/proxy/postprocessor/BasicTest.java
@@ -0,0 +1,52 @@
+package haro.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BasicTest {
+    @Test
+    void basicConfig() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasicConfig.class);
+
+        A a = applicationContext.getBean("beanA", A.class);
+        a.helloA();
+
+//        B b = applicationContext.getBean("beanB", B.class);
+//        b.helloB();
+//
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(B.class));
+    }
+
+    @Slf4j
+    @Configuration
+    static class BasicConfig {
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+
+//        @Bean(name = "beanB")
+//        public B b() {
+//            return new B();
+//        }
+    }
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+    @Slf4j
+    static class B {
+        public void helloB() {
+            log.info("hello B");
+        }
+    }
+}
