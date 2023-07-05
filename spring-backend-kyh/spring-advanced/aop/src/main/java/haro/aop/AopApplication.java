fb3961af1695ed402bbd6262850185647aea8f30
JO <71416000+insightp25@users.noreply.github.com>
2023-07-05 17:56:38 +0900

    Spring AOP init

--- spring-backend-kyh/spring-advanced/aop/src/main/java/haro/aop/AopApplication.java
+++ spring-backend-kyh/spring-advanced/aop/src/main/java/haro/aop/AopApplication.java
@@ -1,4 +1,4 @@
-package hello.aop;
+package haro.aop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-backend-kyh/spring-advanced/aop/src/test/java/haro/aop/AopApplicationTests.java
+++ spring-backend-kyh/spring-advanced/aop/src/test/java/haro/aop/AopApplicationTests.java
@@ -1,4 +1,4 @@
-package hello.aop;
+package haro.aop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
