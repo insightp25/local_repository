3ee354352163e154a015cf4f2a0e058c4eb591c3
insightp25 <insightplug@gmail.com>
2024-05-19 22:22:56 +0900

    init commit

---
+++ etc/recaps/src/test/java/com/istack/recaps/RecapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.istack.recaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
