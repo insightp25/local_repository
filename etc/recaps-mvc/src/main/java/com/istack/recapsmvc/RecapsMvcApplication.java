6249048ac3dee10a7c13d8c14588075cfcff415f
insightp25 <insightplug@gmail.com>
2024-05-19 23:59:25 +0900

    mvc test init

---
+++ etc/recaps-mvc/src/main/java/com/istack/recapsmvc/RecapsMvcApplication.java
@@ -0,0 +1,13 @@
+package com.istack.recapsmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapsMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecapsMvcApplication.class, args);
+	}
+
+}
---
+++ etc/recaps-mvc/src/test/java/com/istack/recapsmvc/RecapsMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.istack.recapsmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapsMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
