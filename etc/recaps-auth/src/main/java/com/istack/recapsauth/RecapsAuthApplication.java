9ac2edfc45eb972a7babc6cc339e5f9ac7ebb542
JO <71416000+insightp25@users.noreply.github.com>
2024-05-22 03:22:56 +0900

    sample project init

---
+++ etc/recaps-auth/src/main/java/com/istack/recapsauth/RecapsAuthApplication.java
@@ -0,0 +1,14 @@
+package com.istack.recapsauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class RecapsAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecapsAuthApplication.class, args);
+	}
+
+}
---
+++ etc/recaps-auth/src/test/java/com/istack/recapsauth/RecapsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.istack.recapsauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapsAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
