995fc8a8bd05f7f02c4d1a853c9a18e0891aefc6
JO <71416000+insightp25@users.noreply.github.com>
2024-06-12 00:57:14 +0900

    user service update, verification 코드 추가

--- TDD/course-review-notes-for-devs-considering-test-application-to-java-spring/test-code-with-architecture-main/src/main/java/com/example/demo/repository/UserEntity.java
+++ TDD/course-review-notes-for-devs-considering-test-application-to-java-spring/test-code-with-architecture-main/src/main/java/com/example/demo/repository/UserEntity.java
@@ -19,7 +19,7 @@ import lombok.Setter;
 public class UserEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "email")
--- TDD/course-review-notes-for-devs-considering-test-application-to-java-spring/test-code-with-architecture-main/src/test/java/com/example/demo/service/UserServiceTest.java
+++ TDD/course-review-notes-for-devs-considering-test-application-to-java-spring/test-code-with-architecture-main/src/test/java/com/example/demo/service/UserServiceTest.java
@@ -5,9 +5,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 
+import com.example.demo.exception.CertificationCodeNotMatchedException;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.UserStatus;
 import com.example.demo.model.dto.UserCreateDto;
+import com.example.demo.model.dto.UserUpdateDto;
 import com.example.demo.repository.UserEntity;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
@@ -22,20 +24,20 @@ import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-//@SqlGroup({
-//    @Sql(value = "/sql/user-repository-test-data.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD),
-//    @Sql(value = "/sql/delete-all-data.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
-//})
-@Transactional
+@SqlGroup({
+    @Sql(value = "/sql/user-service-test-data.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD),
+    @Sql(value = "/sql/delete-all-data.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+})
+//@Transactional
 public class UserServiceTest {
 
     @Autowired
     private UserService userService;
 
-//    @Autowired
+    @Autowired
     @MockBean
     private JavaMailSender mailSender;
-    
+
     @Test
     public void getByEmail_은_ACTIVE_상태인_유저를_찾아올_수_있다() {
         // given
@@ -88,6 +90,17 @@ public class UserServiceTest {
         }).isInstanceOf(ResourceNotFoundException.class);
     }
 
+    //error 발생:
+    //2024-06-12T00:33:10.266+09:00  WARN 92339 --- [           main] o.h.engine.jdbc.spi.SqlExceptionHelper   : SQL Error: 23505, SQLState: 23505
+    //2024-06-12T00:33:10.266+09:00 ERROR 92339 --- [           main] o.h.engine.jdbc.spi.SqlExceptionHelper   : Unique index or primary key violation: "PRIMARY KEY ON PUBLIC.USERS(ID) ( /* key:1 */ CAST(1 AS BIGINT), 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa', 'jos@gmail.com', CAST(0 AS BIGINT), 'jos', 'ACTIVE')"; SQL statement:
+    //insert into users (id, address, certification_code, email, last_login_at, nickname, status) values (default, ?, ?, ?, ?, ?, ?) [23505-214]
+    //
+    //org.springframework.dao.DataIntegrityViolationException: could not execute statement; SQL [n/a]; constraint ["PRIMARY KEY ON PUBLIC.USERS(ID) ( /* key:1 */ CAST(1 AS BIGINT), 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa', 'jos@gmail.com', CAST(0 AS BIGINT), 'jos', 'ACTIVE')"; SQL statement:
+    //insert into users (id, address, certification_code, email, last_login_at, nickname, status) values (default, ?, ?, ?, ?, ?, ?) [23505-214]]
+    //
+    //@MockBean
+    //private JavaMailSender mailSender;
+    //위 코드가 있을시 통과, 없을시 Authentication fail
     @Test
     public void create_를_이용하여_유저를_생성할_수_있다() {
         // given
@@ -96,17 +109,60 @@ public class UserServiceTest {
             .address("Gyeongi")
             .nickname("jos3")
             .build();
-        //BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+        BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
 
-        // @Sql 제거하면 user entity의 generation type 상관없이 다 정상 작동하고, 아니면 PK 1이 이미 존재한다고 data integrity exception 발생.
-        // BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
-        // 이것 없이도 다 통과(원래는 실패해야 정상)
         // when
-        UserEntity result = userService.create(userCreateDto);
+        UserEntity result = userService.create(userCreateDto); // FIXME
 
         // then
         assertThat(result.getId()).isNotNull();
         assertThat(result.getStatus()).isEqualTo(UserStatus.PENDING);
-//        assertThat(result.getCertificationCode()).isEqualTo("TT");
+        //assertThat(result.getCertificationCode()).isEqualTo("TT"); // FIXME
+    }
+
+    @Test
+    public void update_를_이용하여_유저를_생성할_수_있다() {
+        // given
+        UserUpdateDto userUpdateDto = UserUpdateDto.builder()
+                .address("Jeju")
+                .nickname("jos9")
+                .build();
+
+        userService.update(1, userUpdateDto);
+
+        // then
+        UserEntity userEntity = userService.getById(1);
+        assertThat(userEntity.getId()).isNotNull();
+        assertThat(userEntity.getNickname()).isEqualTo("jos9");
+        assertThat(userEntity.getAddress()).isEqualTo("Jeju");
+    }
+
+    @Test
+    public void user_를_로그인_시키면_마지막_로그인_시간이_변경된다() {
+        // given & when
+        userService.login(1);
+
+        // then
+        UserEntity userEntity = userService.getById(1);
+        assertThat(userEntity.getLastLoginAt()).isGreaterThan(0L);
+        //assertThat(userEntity.getLastLoginAt()).isGreaterThan("TT"); // FIXME
+    }
+
+    @Test
+    public void PENDING_상태의_사용자는_인증_코드로_ACTIVE_시킬_수_있다() {
+        // given & when
+        userService.verifyEmail(2, "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaab");
+
+        // then
+        UserEntity userEntity = userService.getById(2);
+        assertThat(userEntity.getStatus()).isEqualTo(UserStatus.ACTIVE);
+    }
+
+    @Test
+    public void PENDING_상태의_사용자는_잘못된_인증_코드를_받으면_에러를_던진다() {
+        // given & when & then
+        assertThatThrownBy(() -> {
+            userService.verifyEmail(2, "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaC");
+        }).isInstanceOf(CertificationCodeNotMatchedException.class);
     }
 }
