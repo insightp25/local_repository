4fa772ac3968f439bb62b1440b4ad0226c5a9bcc
JO <71416000+insightp25@users.noreply.github.com>
2024-06-13 03:12:31 +0900

    user controller 인증 실패 테스트 추가

--- TDD/course-review-notes-for-devs-considering-test-application-to-java-spring/test-code-with-architecture-main/src/test/java/com/example/demo/controller/UserControllerTest.java
+++ TDD/course-review-notes-for-devs-considering-test-application-to-java-spring/test-code-with-architecture-main/src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -68,6 +68,15 @@ public class UserControllerTest {
     }
 
     @Test
+    void 사용자는_인증_코드가_일치하지_않을_경우_권한_없음_에러를_내려준다() throws Exception {
+        mockMvc.perform(
+                get("/api/users/2/verify")
+                        .queryParam("certificationCode", "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaD"))
+                .andExpect(status().isForbidden())
+                .andExpect(content().string("자격 증명에 실패하였습니다."));
+    }
+
+    @Test
     void 사용자는_내_정보를_불러올_때_개인정보인_주소를_갖고_올_수_있다() throws Exception {
         mockMvc.perform(
                 get("/api/users/me")
