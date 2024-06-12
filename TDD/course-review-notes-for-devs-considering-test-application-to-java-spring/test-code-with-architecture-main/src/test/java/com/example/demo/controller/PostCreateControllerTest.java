7980d1db9fcb629de8a9c82f0797c723cece14e0
JO <71416000+insightp25@users.noreply.github.com>
2024-06-13 03:03:07 +0900

    posts 관련 controller 테스트 추가

--- TDD/course-review-notes-for-devs-considering-test-application-to-java-spring/test-code-with-architecture-main/src/test/java/com/example/demo/controller/PostCreateControllerTest.java
+++ TDD/course-review-notes-for-devs-considering-test-application-to-java-spring/test-code-with-architecture-main/src/test/java/com/example/demo/controller/PostCreateControllerTest.java
@@ -1,4 +1,53 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.dto.PostCreateDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/user-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
 public class PostCreateControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void 사용자는_게시물을_작성할_수_있다() throws Exception {
+        // given
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .writerId(99L)
+                .content("created post content1")
+                .build();
+
+        // when
+        // then
+        mockMvc.perform(
+                post("/api/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postCreateDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.content").value("created post content1"))
+                .andExpect(jsonPath("$.writer.id").value(99L))
+                .andExpect(jsonPath("$.writer.email").value("jos@gmail.com"))
+                .andExpect(jsonPath("$.writer.nickname").value("jos"));
+    }
 }
