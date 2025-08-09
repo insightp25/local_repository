56f93d33689f9166bb8e1d22fa8e3cfb532fe4c4
JO <71416000+insightp25@users.noreply.github.com>
2025-08-10 00:11:44 +0900

    polishing

--- TDD/course-review-notes-for-devs-considering-test-application-to-java-spring/test-code-with-architecture-main/src/test/java/com/example/demo/medium/PostControllerTest.java
+++ TDD/course-review-notes-for-devs-considering-test-application-to-java-spring/test-code-with-architecture-main/src/test/java/com/example/demo/medium/PostControllerTest.java
@@ -21,8 +21,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
 @SqlGroup({
-        @Sql(value = "/sql/post-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
-        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql(value = "/sql/post-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+    @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 })
 public class PostControllerTest {
 
@@ -34,35 +34,35 @@ public class PostControllerTest {
     @Test
     void 사용자는_특정_게시물의_정보를_전달_받을_수_있다() throws Exception {
         mockMvc.perform(get("/api/posts/99"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(99L))
-                .andExpect(jsonPath("$.content").value("written content1"))
-                .andExpect(jsonPath("$.writer.id").value(99L))
-                .andExpect(jsonPath("$.writer.email").value("jos@gmail.com"))
-                .andExpect(jsonPath("$.writer.nickname").value("jos"));
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value(99L))
+            .andExpect(jsonPath("$.content").value("written content1"))
+            .andExpect(jsonPath("$.writer.id").value(99L))
+            .andExpect(jsonPath("$.writer.email").value("jos@gmail.com"))
+            .andExpect(jsonPath("$.writer.nickname").value("jos"));
     }
 
     @Test
     void 사용자가_존재하지_않는_게시물의_정보를_조회할_경우_에러가_발생한다() throws Exception {
         mockMvc.perform(get("/api/posts/123456789"))
-                .andExpect(status().isNotFound())
-                .andExpect(content().string("Posts에서 ID 123456789를 찾을 수 없습니다."));
+            .andExpect(status().isNotFound())
+            .andExpect(content().string("Posts에서 ID 123456789를 찾을 수 없습니다."));
     }
 
     @Test
     void 사용자는_게시물_정보를_수정할_수_있다() throws Exception {
         PostUpdate postUpdate = PostUpdate.builder()
-                .content("test modification1")
-                .build();
+            .content("test modification1")
+            .build();
 
         mockMvc.perform(put("/api/posts/99")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(postUpdate)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(99L))
-                .andExpect(jsonPath("$.content").value("test modification1"))
-                .andExpect(jsonPath("$.writer.id").value(99L))
-                .andExpect(jsonPath("$.writer.email").value("jos@gmail.com"))
-                .andExpect(jsonPath("$.writer.nickname").value("jos"));
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(postUpdate)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value(99L))
+            .andExpect(jsonPath("$.content").value("test modification1"))
+            .andExpect(jsonPath("$.writer.id").value(99L))
+            .andExpect(jsonPath("$.writer.email").value("jos@gmail.com"))
+            .andExpect(jsonPath("$.writer.nickname").value("jos"));
     }
 }
